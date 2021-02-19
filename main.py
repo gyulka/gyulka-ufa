a792eaf1c5f15aa2d5152e9e3777acefc8766449
__gyulka__ <romagyuli-zade@yandex.ru>
2021-02-19 18:11:25 +0500

    задача номер 3

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import random
 import sys
 
-from PyQt5 import QtGui
-from PyQt5 import QtWidgets
-from PyQt5 import uic
-
-Ui_MainWindown, _ = uic.loadUiType('untitled.ui')
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(Ui_MainWindown,QtWidgets.QMainWindow):
+# Ui_MainWindown, _ = uic.loadUiType('untitled.ui')
+
+class Ui_MainWindown(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "кнопка"))
+
+
+class Ui_MainWindow(Ui_MainWindown, QtWidgets.QMainWindow):
     def init2(self):
         self.paint = None
         self.coords = (None, None)
@@ -34,7 +55,7 @@ class Ui_MainWindow(Ui_MainWindown,QtWidgets.QMainWindow):
 
     def drawcircle(self, qp):
         if self.coords[0] is not None:
-            color = QtGui.QColor(255,255,0)
+            color = QtGui.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
             qp.setBrush(color)
             x = random.randint(5, 30)
             qp.drawEllipse(self.coords[0] - x, self.coords[1] - x, 2 * x, 2 * x, )
